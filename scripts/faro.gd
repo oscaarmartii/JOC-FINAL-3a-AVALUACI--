f143759166a0d482d27d14a8567318c6600eb502
Alex Boya <alexboya99@gmail.com>
2024-04-23 17:57:02 +0200

    ara els faros fan la llum aleatoriament

--- scripts/faro.gd
+++ scripts/faro.gd
@@ -3,8 +3,8 @@ extends Node2D
 @onready var animacio: AnimationPlayer = $AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#animacio.seek(randf_range(0,5), true)
 	animacio.play("faro")
+	animacio.seek(randf_range(0,2), true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
