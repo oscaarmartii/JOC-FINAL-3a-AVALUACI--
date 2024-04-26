217ca7dbfbc7ed91d04569b4069f3b2c50c80ae4
alexboyaa <118176566+alexboyaa@users.noreply.github.com>
2024-04-26 14:07:56 +0200

    es pot disparar

--- escenes/bala.gd
+++ escenes/bala.gd
@@ -1,13 +1,20 @@
 extends Area2D
 
-var direccio:Vector2 = Vector2.RIGHT 
+var direccio =  global_position.direction_to(get_global_mouse_position())
 var velocitat := 300
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position += direccio * velocitat * delta
-	
+
+
+@onready var animacio: AnimationPlayer = $AnimationPlayer
+
+func _ready():
+	rotate(direccio.angle())
+	animacio.play("bala")
+	animacio.seek(randf_range(0,2), true)
 
 func _on_timer_timeout():
 	queue_free()
