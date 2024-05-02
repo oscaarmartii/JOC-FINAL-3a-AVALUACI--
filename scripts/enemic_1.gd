0e972740c8981aeae335dbdc1f0d510e4cff0b65
oscaarmartii <165768506+oscaarmartii@users.noreply.github.com>
2024-05-02 08:58:18 +0200

    animacio del enemic

--- scripts/enemic_1.gd
+++ scripts/enemic_1.gd
@@ -10,6 +10,7 @@ func _physics_process(delta):
 		print(canvi)
 		velocity = global_position.direction_to(zona.global_position) * speed
 		move_and_slide()
+		play_anim()
 	
 
 func _on_area_2d_body_entered(body):
@@ -20,14 +21,12 @@ func _on_area_2d_body_exited(body):
 	zona = null
 	
 func play_anim():
-	var dir = zona
 	var anim = $AnimatedSprite2D
 	if velocity.length() > 0:
-		anim.play("running")
+		anim.play("run")
 	else:
 		anim.play("idle")
-			
-			
+		
 	if velocity.x > 0:
 		anim.flip_h = false
 	if velocity.x < 0:
