beeb5ccb8102761e9e576199cb7efbcb7fba6e90
Alex Boya <alexboya99@gmail.com>
2024-05-23 10:33:26 +0200

    vidas fetes

--- global.gd
+++ global.gd
@@ -1,9 +1,5 @@
 extends Node
 
-var vidas = 3:
-	set(novas_vidas):
-		vidas = novas_vidas
-		UI.get_node("VboxContainer/Label").text = "vidas: %d" % [vidas]
-	
+var vidas = 3
 var UI : CanvasLayer
 var Jugador:CharacterBody2D
